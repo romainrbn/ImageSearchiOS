ecebcad0ee7cd9471a5c7cb633ada8616275a40f
Romain Rabouan <romain@bim.miami>
2022-07-06 23:11:07 +0200

    Fixed unexpected character

--- ImageSearch/Controllers/DetailViewController/DetailCollectionViewController.swift
+++ ImageSearch/Controllers/DetailViewController/DetailCollectionViewController.swift
@@ -13,16 +13,6 @@ class DetailCollectionViewController: UICollectionViewController {
     
     var selectedImages: [String]?
     
-    init(selectedImages: [String], layout: UICollectionViewLayout) {
-        self.selectedImages = selectedImages
-        
-        super.init(collectionViewLayout: layout)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
